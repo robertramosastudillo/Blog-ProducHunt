8c091c302e73089c6dbd2213c470026c7c151eea
robertramosastudillo <robertramosastudillo@gmail.com>
2020-09-30 20:57:45 -0500

    Se comenzo con la integracion de firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzED8SMaNDvXzpDZl0NfLkjhBGkBvPNrA",
+  authDomain: "product-hunt-ef3c1.firebaseapp.com",
+  databaseURL: "https://product-hunt-ef3c1.firebaseio.com",
+  projectId: "product-hunt-ef3c1",
+  storageBucket: "product-hunt-ef3c1.appspot.com",
+  messagingSenderId: "240537111020",
+  appId: "1:240537111020:web:6bc5adaae5d51f3a6858c9",
+};
+
+export default firebaseConfig;
--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,42 +1,93 @@
 import React, { Fragment } from "react";
+import { css } from "@emotion/core";
 import Layout from "../components/layouts/Layout";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
+
+// Validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
+
+const STATE_INICIAL = {
+  nombre: "",
+  email: "",
+  password: "",
+};
 
 const CrearCuenta = () => {
+  const {
+    valores,
+    errores,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useValidacion(STATE_INICIAL, validarCrearCuenta, CrearCuenta);
+
+  const { nombre, email, password } = valores;
+
+  function crearCuenta() {
+    console.log("Creando cuenta...");
+  }
+
   return (
     <div>
       <Layout>
         <Fragment>
-          <h1>Crear Cuenta</h1>
-          <form action="">
-            <div>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Crear Cuenta
+          </h1>
+          <Formulario onSubmit={handleSubmit} noValidate>
+            <Campo>
               <label htmlFor="nombre">Nombre</label>
               <input
                 type="text"
                 name="nombre"
                 id="nombre"
                 placeholder="Tu Nombre"
+                value={nombre}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
-            </div>
-            <div>
+            </Campo>
+            {errores.nombre && <Error>{errores.nombre}</Error>}
+
+            <Campo>
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
                 id="email"
                 placeholder="Tu Email"
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
-            </div>
-            <div>
+            </Campo>
+            {errores.email && <Error>{errores.email}</Error>}
+            <Campo>
               <label htmlFor="password">Password</label>
               <input
                 type="password"
                 name="password"
                 id="password"
                 placeholder="Tu Password"
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
-            </div>
-            <input type="submit" value="Crear Cuenta"/>
-          </form>
+            </Campo>
+            {errores.password && <Error>{errores.password}</Error>}
+            <InputSubmit type="submit" value="Crear Cuenta" />
+          </Formulario>
         </Fragment>
       </Layout>
     </div>
